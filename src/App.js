1ec6bd157a1016f734b9ec5a5f267adef3df5887
AbrahamOsco <anaester.gonzalez@live.com>
2024-01-17 01:36:16 -0300

    Mensaje de tu commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ProductList from './components/ProductList.jsx'; // Asegúrate de que la ruta sea correcta
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Barra de navegación */}
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container">
+          <span className="navbar-brand mb-0 h1">Mi Tienda </span>
+        </div>
+      </nav>
+
+      {/* Contenido principal */}
+      <div className="container mt-4">
+        <ProductList />
+      </div>
+
+      {/* Pie de página */}
+      <footer className="bg-dark text-light text-center py-3">
+        © 2024 Indumentaria-Accesorio
+      </footer>
     </div>
   );
 }
